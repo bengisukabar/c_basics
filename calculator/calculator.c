c39f5c9c3520f37e7e3229311a1b84eb7d1e0fb1
bengisukabar <bengisukbr@outlook.com>
2026-01-26 00:44:59 +0300

    refactor calculator using functions

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -1,64 +1,84 @@
 #include <stdio.h>
 
+double addition(double x, double y);
+double subtraction(double x, double y);
+double multiplication(double x, double y);
+double division(double x, double y);
 int main() {
     int choice;
     double num1, num2,result;
-
-    printf("--------Calculator----------\n");
-    printf("Welcome to the Calculator!\n");
-    printf("1.Addition\n");
-    printf("2.Subtraction\n");
-    printf("3.Multiplication\n");
-    printf("4.Division\n");
-    printf("0.Exit\n");
-
-    printf("Enter your choice: ");
-    scanf("%d",&choice);
-
-    switch (choice)
+    do
     {
-        case 1:printf("Addition selected \n");
-        printf("Enter first number:");
-        scanf("%lf",&num1);
-        printf("Enter second number:");
-        scanf("%lf",&num2);
-        result = num1 + num2;
-        printf("Result of Addition is %.2lf",result);
-        break;
-        case 2:printf("Subtraction selected \n");
-        printf("Enter first number:");
-        scanf("%lf",&num1);
-        printf("Enter second number:");
-        scanf("%lf",&num2);
-        result = num1 - num2;
-        printf("Result of Subtraction is %.2lf",result);
-        break;
-        case 3:printf("Multiplication selected \n");
-        printf("Enter first number:");
-        scanf("%lf",&num1);
-        printf("Enter second number:");
-        scanf("%lf",&num2);
-        result = num1 * num2;
-        printf("Result of Multiplication is %.2lf",result);
-        break;
-        case 4:printf("Division selected \n");
-        printf("Enter first number:");
-        scanf("%lf",&num1);
-        printf("Enter second number:");
-        scanf("%lf",&num2);
-        if (num2==0)
+        printf("--------Calculator----------\n");
+        printf("Welcome to the Calculator!\n");
+        printf("1.Addition\n");
+        printf("2.Subtraction\n");
+        printf("3.Multiplication\n");
+        printf("4.Division\n");
+        printf("0.Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+        if (choice == 0)
         {
-            printf("Error: Division by zero is not allowed\n");
+            printf("Exiting...\n:");
             break;
         }
-        result = num1 / num2;
-        printf("Result of Division is %.2lf",result);
+        if (choice <0 || choice > 4)
+        {
+            printf("Please enter a valid choice!\n");
+            continue;
+        }
+        printf("Enter the first number: ");
+        scanf("%lf",&num1);
+        printf("Enter the second number: ");
+        scanf("%lf",&num2);
 
-        break;
-        case 0:printf("Exit\n");
-        break;
-        default:printf("Invalid choice \n");
+        switch (choice)
+        {
+        case 1:printf("Addition selected \n");
+
+            result = addition(num1, num2);
+            printf("Result of Addition is %.2lf\n",result);
+            break;
+        case 2:printf("Subtraction selected \n");
+            result = subtraction(num1, num2);
+            printf("Result of Subtraction is %.2lf \n",result);
+            break;
+        case 3:printf("Multiplication selected \n");
+            result = multiplication(num1, num2);
+            printf("Result of Multiplication is %.2lf \n",result);
+            break;
+        case 4:printf("Division selected \n");
+
+            if (num2==0)
+            {
+                printf("Error: Division by zero is not allowed\n");
+                break;
+            }else{
+                result = division(num1, num2);
+                printf("Result of Division is %.2lf\n",result);
+            }
+            break;
+        }
+
+        }while (1);
+
+        return 0;
     }
-
-    return 0;
+double addition (double x, double y)
+{
+    return x+y;
+}
+double subtraction(double x, double y)
+{
+    return x-y;
+}
+double multiplication (double x, double y)
+{
+    return x*y;
+}
+double division (double x, double y)
+{
+    return x/y;
 }
